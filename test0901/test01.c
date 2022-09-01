9c51289dd9e16eb86ffc25a6d8c873fe1f0160a7
彦浩 刘 <1058542590@qq.com>
2022-09-01 14:56:57 +0800

    添加项目文件。

---
+++ test0901/test01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	/*printf("c:\test\test");
+	printf("\nc:\\test\\test");
+	printf("\n(are you ok??)");
+	printf("%s\n","\"");
+	printf("%s\n","abc\b");
+	printf("\n\nabc\r");
+	printf("\v\v\v");*/
+	printf("%d\n", strlen("c:\test\328\test.c"));
+	return 0;
+}
